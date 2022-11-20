0933c99666c37e2e3c07512ad727f3f762086de3
Dmitry_Kulik <dima27121985@gmail.com>
2022-11-20 19:19:32 +0300

    hurray

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+
+let products = 0;
+let totalPrice = 0;
+let arrayOfItems = [];
+
+
 fetch('https://dummyjson.com/products')
   .then(res => res.json())
   .then((data) => {
     console.log(data.products);
-    // Start here :)
-    const products = data.products; 
+
+    products = data.products; 
     const div = document.querySelector('.wrapper');
     div.classList.add('gy-3'); 
 
@@ -11,6 +17,7 @@ fetch('https://dummyjson.com/products')
 
       const cardContainer = document.createElement('div');
       cardContainer.classList.add('col-md-3');
+      cardContainer.setAttribute('id', data.id);
 
       const productCard = document.createElement('div');
       productCard.classList.add('card');
@@ -19,7 +26,7 @@ fetch('https://dummyjson.com/products')
 
       const img = document.createElement(`img`);
       img.setAttribute('src', data.thumbnail);
-      img.setAttribute('alt', `Image of product`);
+      img.setAttribute('alt', data.title);
       img.style.objectFit = 'cover';
       img.style.height = '180px';
 
@@ -48,9 +55,16 @@ fetch('https://dummyjson.com/products')
       const rating = document.createElement('div');
       rating.textContent = `Rating : ${data.rating}`;
 
+      const buttonAddToCart = document.createElement('a');
+      buttonAddToCart.classList.add('btn');
+      buttonAddToCart.classList.add('btn-primary');
+      buttonAddToCart.classList.add('mt-2');
+      buttonAddToCart.textContent = 'Add to cart';
+
       bodyProductCard.append(title);
       bodyProductCard.append(description);
-      bodyProductCard.append(footerProductCard);  
+      bodyProductCard.append(footerProductCard); 
+      bodyProductCard.append(buttonAddToCart); 
       
       footerProductCard.append(price);
       footerProductCard.append(rating);
@@ -61,6 +75,48 @@ fetch('https://dummyjson.com/products')
       cardContainer.append(productCard);
 
       div.append(cardContainer);
-      
+
+      const buttonCart = document.querySelector('.buttonCart');
+      buttonCart.textContent = 'You cart - ' + totalPrice + '.00$'; 
+
+      buttonAddToCart.onclick = function addPrice(Element) {
+        totalPrice += data.price;
+        return  buttonCart.textContent = 'You cart - ' + totalPrice + '.00$'; 
+      }
+
+      buttonCart.onclick = function openCart(Element) {
+        const shoppingCart = document.querySelector('.shoppingCart');
+
+        if(shoppingCart.style.display === `block` ){
+          shoppingCart.style.display = `none`; 
+        }else {
+          shoppingCart.style.display = `block`; 
+        }
+      }
     });
   });
+
+  function findItems(Element) {
+    let valueofSearch = document
+    .getElementsByName("q")[0].value
+    .toLowerCase()
+    .trim();
+   
+    products.forEach(data => {
+      let descriptionValue = data.description
+      .toLowerCase()
+      .indexOf(valueofSearch);
+
+      let titleValue = data.title
+      .toLowerCase()
+      .indexOf(valueofSearch);
+
+      let currentDiv = document.getElementById(data.id);
+
+      if (descriptionValue >= 0 || titleValue >= 0) {
+        currentDiv.style.display = `block`; 
+      }else {
+        currentDiv.style.display = `none`; 
+      }
+    });
+ }
